9f18c473025f149f701a7f73e165cab499e4e9d7
Mohammad Momeni <lo3me@pm.me>
2023-05-07 17:18:19 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # push-swap-tester
 Some tests for 42 push_swap project
+
+# Requirements
+`checker_Mac` should be present in your project directory
+
+# Usage
+```
+python3 test.py
+```
+It checks all the combination of 3 and 5 numbers for you
+
+# Todo
+- [ ] Add tests for 100 and 500 numbers
---
+++ test.py
@@ -0,0 +1,40 @@
+from itertools import permutations
+import subprocess
+
+
+def test(n: int, max_instructions: int):
+    ns = [i for i in range(n)]
+    perms = permutations(ns)
+    failed = []
+    for perm in perms:
+        arg = " ".join([str(i) for i in perm])
+        print(arg)
+        ps = subprocess.run(["../push_swap", arg], capture_output=True)
+        check = subprocess.run(
+            ["../checker_Mac", arg], input=ps.stdout, capture_output=True
+        )
+        check = check.stdout.decode("utf-8")
+        o = ps.stdout.decode("utf-8").strip().split("\n")
+        print(" ".join(o) if o[0] else "(already sorted)")
+        print("Actions:", len(o) if o[0] else 0)
+        print("Result:", check)
+        if check == "KO" or len(o) > max_instructions:
+            failed.append(arg)
+    if failed:
+        print(len(failed), "Tests failed ❌")
+        for t in failed:
+            print(t)
+    else:
+        print(
+            "Congrats! all tests passed ✅\nYour push_swap works perfectly for any combination of",
+            n,
+            "numbers\n",
+        )
+
+
+if __name__ == "__main__":
+    test(3, 3)
+    c = input("Do you want to test 5 numbers as well? [y/n]: ")
+    if c.lower() == "y" or c == "":
+        test(5, 12)
+        
