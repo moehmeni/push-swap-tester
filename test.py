42027d71d98781142b009248d4a42a9dc94c8929
Mohammad Momeni <lo3me@pm.me>
2023-05-30 17:44:22 +0200

    Fix checker failure bug

--- test.py
+++ test.py
@@ -13,17 +13,19 @@ def test(n: int, max_instructions: int):
         check = subprocess.run(
             ["../checker_Mac", arg], input=ps.stdout, capture_output=True
         )
-        check = check.stdout.decode("utf-8")
+        check = check.stdout.decode("utf-8").strip()
         o = ps.stdout.decode("utf-8").strip().split("\n")
         print(" ".join(o) if o[0] else "(already sorted)")
         print("Actions:", len(o) if o[0] else 0)
         print("Result:", check)
+        print("----------------------")
         if check == "KO" or len(o) > max_instructions:
             failed.append(arg)
     if failed:
         print(len(failed), "Tests failed ❌")
         for t in failed:
             print(t)
+        print("----------------------\n")
     else:
         print(
             "Congrats! all tests passed ✅\nYour push_swap works perfectly for any combination of",
